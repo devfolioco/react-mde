b1c7fe77b9e89baf09f9dfae7685e7dd04a309b8
Tushar Choudhari <33191895+chtushar@users.noreply.github.com>
2022-06-30 16:12:05 +0530

    :ambulance: Import only debounce from lodash

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { debounce } from "lodash";
+import debounce from "lodash/debounce";
 import { classNames, ClassValue } from "../util/ClassNames";
 import {
   CaretCoordinates,
